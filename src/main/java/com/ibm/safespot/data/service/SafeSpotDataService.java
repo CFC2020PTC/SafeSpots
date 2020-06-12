c29c416258469fbed07d743fbb12c1c02a430305
Prabhat Kumar <prabhat.librian@gmail.com>
2020-06-12 10:35:15 +0530

    Rest API updated

--- src/main/java/com/ibm/safespot/data/service/SafeSpotDataService.java
+++ src/main/java/com/ibm/safespot/data/service/SafeSpotDataService.java
@@ -49,6 +49,14 @@ public class SafeSpotDataService extends _ServiceManager{
 			safeSpot.setName((String) queryResult.get("name"));
 			safeSpot.setLatLong((String) queryResult.get("LatLong"));
 			safeSpot.setType((String) queryResult.get("type"));
+			
+			safeSpot.setAddress((String) queryResult.get("address"));
+			safeSpot.setOverAllsanitization((Number) queryResult.get("overAllsanitization"));
+			safeSpot.setOverAlltemperature_sensor((Number) queryResult.get("overAlltemperature_sensor"));
+			safeSpot.setOverAllsocial_distancing((Number) queryResult.get("overAllsocial_distancing"));
+			safeSpot.setOverAllmask((Number) queryResult.get("overAllmask"));
+			safeSpot.setOverAllRating((Number) queryResult.get("overAllRating"));
+
 			safeSpot.setSaftyReview((ArrayList<SafeSpotReviews>) queryResult.get("safty_review"));
 			safeSpots.add(safeSpot);
 		}
--- src/main/java/com/ibm/safespot/model/SafeSpot.java
+++ src/main/java/com/ibm/safespot/model/SafeSpot.java
@@ -20,8 +20,30 @@ public class SafeSpot {
 	private String LatLong;
 	@SerializedName("city")
 	private String city;
+	
+	@SerializedName("address")
+	private String address;
+	
+	@SerializedName("overAllsanitization")
+	private Number overAllsanitization;
+	@SerializedName("overAlltemperature_sensor")
+	private Number overAlltemperature_sensor;
+	@SerializedName("overAllsocial_distancing")
+	private Number overAllsocial_distancing;
+	@SerializedName("overAllmask")
+	private Number overAllmask;
+	@SerializedName("overAllRating")
+	private Number overAllRating;
+	
 	@SerializedName("safty_review")
 	private List<SafeSpotReviews> saftyReview;
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	
 	public List<SafeSpotReviews> getSaftyReview() {
 		return saftyReview;
@@ -76,5 +98,35 @@ public class SafeSpot {
 	 */
 	public void setType(String type) {
 		this.type = type;
+	}
+	public Number getOverAllsanitization() {
+		return overAllsanitization;
+	}
+	public void setOverAllsanitization(Number overAllsanitization) {
+		this.overAllsanitization = overAllsanitization;
+	}
+	public Number getOverAlltemperature_sensor() {
+		return overAlltemperature_sensor;
+	}
+	public void setOverAlltemperature_sensor(Number overAlltemperature_sensor) {
+		this.overAlltemperature_sensor = overAlltemperature_sensor;
+	}
+	public Number getOverAllsocial_distancing() {
+		return overAllsocial_distancing;
+	}
+	public void setOverAllsocial_distancing(Number overAllsocial_distancing) {
+		this.overAllsocial_distancing = overAllsocial_distancing;
+	}
+	public Number getOverAllmask() {
+		return overAllmask;
+	}
+	public void setOverAllmask(Number overAllmask) {
+		this.overAllmask = overAllmask;
+	}
+	public Number getOverAllRating() {
+		return overAllRating;
+	}
+	public void setOverAllRating(Number overAllRating) {
+		this.overAllRating = overAllRating;
 	}
 }
